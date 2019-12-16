12d4ad086fb75f8e502bcdf3cc141623b721652f
0x9f99 <0x9f99@gmail.com>
2019-12-17 03:20:57 +0800

    Update vul_check.py

--- vul_check.py
+++ vul_check.py
@@ -8,7 +8,6 @@ from queue import Queue
 vul_list = []
 web_queue = []
 
-
 with open(sys.argv[1], "r") as f:
     for i in f.readlines():
         web_queue.append(i.strip())
@@ -16,16 +15,15 @@ with open(sys.argv[1], "r") as f:
 
 for vulClass in [shiro,weblogic,struts2]:
     detect = vulClass.Detect
-    cprint('-' * 50 + 'detect ' + detect.name + '-' * 50, 'green')  # 探测各种漏洞
     try:
-        alive_Web_queue = Queue(-1)  # 将存活的web存入队列里
+        alive_Web_queue = Queue(-1)
         for _ in web_queue:
             alive_Web_queue.put(_)
 
         threads = []
-        thread_num = 50  # 漏洞检测的线程数目
+        thread_num = 50
         for num in range(1, thread_num + 1):
-            t = detect(alive_Web_queue, vul_list)  # 实例化漏洞类，传递参数：存活web的队列，  存储漏洞的列表
+            t = detect(alive_Web_queue, vul_list)
             threads.append(t)
             t.start()
         for t in threads:
