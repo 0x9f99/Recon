c0a40ef0042b224c7a15aa2f578c1faf877cf60f
0x9f99 <0x9f99@gmail.com>
2019-12-17 00:40:28 +0800

    Update vul_check.py

--- vul_check.py
+++ vul_check.py
@@ -24,5 +24,9 @@ shiro=threading.Thread(target=shiro_check.run)
 weblogic_check = weblogic.Detect(alive_Web_queue,vul_list)
 weblogic=threading.Thread(target=weblogic_check.run)
 
-shiro.start()
-weblogic.start()
+struts2_check = weblogic.Detect(alive_Web_queue,vul_list)
+struts2=threading.Thread(target=weblogic_check.run)
+
+#shiro.start()
+#weblogic.start()
+struts2.start()
