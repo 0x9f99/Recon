a820b0e9d8b5d3c2fdf37a2334951aed38b1cda6
0x9f99 <0x9f99@gmail.com>
2019-12-17 11:34:31 +0800

    Update Recon.sh

--- Recon.sh
+++ Recon.sh
@@ -16,11 +16,11 @@ RESET="\033[0m"
 checkArgs(){
     if [[ $# -eq 0 ]]; then
         echo -e "\t${RED}[!] ERROR:${RESET} Invalid argument!\n"
-        echo -e "\t${GREEN}[+] USAGE:${RESET}$0 <list-of-IP/CIDR>\n"
+        echo -e "\t${GREEN}[+] USAGE:${RESET}$0 ip.txt\n"
         exit 1
     elif [ ! -s $1 ]; then
         echo -e "\t${RED}[!] ERROR:${RESET} File is empty and/or does not exists!\n"
-        echo -e "\t${GREEN}[+] USAGE:${RESET}$0 <list-of-IP/CIDR>\n"
+        echo -e "\t${GREEN}[+] USAGE:${RESET}$0 ip.txt\n"
         exit 1
     fi
 }
