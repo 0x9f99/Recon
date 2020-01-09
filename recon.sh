cfcac14c16744df7b1ed688eec36e3fd409e492b
0x9f99 <0x9f99@gmail.com>
2020-01-09 19:23:29 +0800

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -19,13 +19,8 @@ checkArgs(){
         echo -e "\t${RED}[!] ERROR:${RESET} Invalid argument!\n"
         echo -e "\t${GREEN}[+] USAGE:${RESET}$0 ip.txt or $0 domain.com\n"
         exit 1
-    elif [ ! -s $1 ]; then
-        echo -e "\t${RED}[!] ERROR:${RESET} File is empty and/or does not exists!\n"
-        echo -e "\t${GREEN}[+] USAGE:${RESET}$0 ip.txt or $0 domain.com\n"
-        exit 1
     elif [ $1 != "ip.txt" ]; then
         EnumSubDomains
-        exit 1        
     fi
 }
 
