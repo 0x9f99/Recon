4629eb02615b69ebedf3285b38fc5267bc5c7d5a
0x9f99 <0x9f99@gmail.com>
2020-03-02 21:49:24 +0800

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -36,13 +36,13 @@ setupTools(){
 
 installTools(){
     
-    echo -e "${RED}[+] Installing the latest version of Go...${RESET}"
-    LATEST_GO=$(wget -qO- https://golang.org/dl/ | grep -oP 'go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2)
-    wget https://dl.google.com/go/go$LATEST_GO.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf go$LATEST_GO.linux-amd64.tar.gz
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-    source ~/.profile
-    rm -rf go$LATEST_GO*
+    #echo -e "${RED}[+] Installing the latest version of Go...${RESET}"
+    #LATEST_GO=$(wget -qO- https://golang.org/dl/ | grep -oP 'go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2)
+    #wget https://dl.google.com/go/go$LATEST_GO.linux-amd64.tar.gz
+    #sudo tar -C /usr/local -xzf go$LATEST_GO.linux-amd64.tar.gz
+    #echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+    #source ~/.profile
+    #rm -rf go$LATEST_GO*
     
     LATEST_MASSCAN="1.0.6"
     if [ ! -x "$(command -v masscan)" ]; then
