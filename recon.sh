3db5f3a41b2d7502b4a4dee34aadb9605686ad14
0x9f99 <0x9f99@gmail.com>
2020-04-08 15:08:01 +0800

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -195,7 +195,7 @@ vulScanner(){
     echo -e "${GREEN}[+] Running vul-scanner.${RESET}"
     python3 $WORKING_DIR/epfa.py url_list | tee  $NRESULTS_PATH/vul_result.txt
     echo -e "${GREEN}[+] Running Eyewitness.${RESET}"
-    sudo -i python3 $WORKING_DIR/EyeWitness/EyeWitness.py -x $NRESULTS_PATH/nmap.xml --no-prompt -d $ERESULTS_PATH  --no-dns --ocr
+    sudo -i python3 $WORKING_DIR/EyeWitness/Python/EyeWitness.py -x $NRESULTS_PATH/nmap.xml --no-prompt -d $ERESULTS_PATH  --no-dns --ocr
     echo -e "${GREEN}[+] Running weak-password-scanner.${RESET}"
     cd $WORKING_DIR/tomcat-weak-password-scanner/ && python koala_tomcat_cmd.py -h ip.txt -p port.txt && cd -
 }
